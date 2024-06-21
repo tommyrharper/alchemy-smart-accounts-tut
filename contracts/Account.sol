9077ea9cad5c786cbce5928b947f9f278c503031
tommyrharper <thomasrobertharper@gmail.com>
2024-06-21 15:07:49 +0100

    feat: account factory

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -5,11 +5,29 @@ import "@account-abstraction/contracts/core/EntryPoint.sol";
 import "@account-abstraction/contracts/interfaces/IAccount.sol";
 
 contract Account is IAccount {
-    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 missingAccountFunds)
-    external
-    pure
-    returns (uint256 validationData) {
+    uint256 public count;
+    address public owner;
+
+    constructor(address owner) {
+        owner = owner;
+    }
+
+    function validateUserOp(
+        UserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external pure returns (uint256 validationData) {
         return 0;
+    }
+
+    function execute() external {
+        count++;
     }
 }
 
+contract AccountFactory {
+    function createAccount(address owner) external returns (address) {
+        Account acc = new Account(owner);
+        return address(acc);
+    }
+}
