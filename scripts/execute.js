0c16d40ad5a5971b9dca76fc9c37c46e9acd1514
tommyrharper <thomasrobertharper@gmail.com>
2024-07-01 13:33:24 +0100

    refactor: add toggle for IS_FIRST_RUN to execute.js

--- scripts/execute.js
+++ scripts/execute.js
@@ -6,6 +6,9 @@ const FACTORY_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
 const EP_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
 const PM_ADDRESS = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
 
+/// @dev make sure to change this appropriately!!!
+const IS_FIRST_RUN = true;
+
 async function main() {
   const [signer0] = await hre.ethers.getSigners();
   const address0 = await signer0.getAddress();
@@ -21,18 +24,24 @@ async function main() {
   });
 
   const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
-  // const initCode = "0x"; // if deploying for first time use init code below
-    const initCode = FACTORY_ADDRESS +
-    AccountFactory.interface
-      .encodeFunctionData("createAccount", [address0])
-      .slice(2);
+  let initCode;
+  if (IS_FIRST_RUN) {
+    // init code is needed if the account hasn't been deployed yet
+    initCode =
+      FACTORY_ADDRESS +
+      AccountFactory.interface
+        .encodeFunctionData("createAccount", [address0])
+        .slice(2);
 
-	console.log("sender", sender);
+    // the entry point will also need funds to pay for the deposit
+    await entryPoint.depositTo(PM_ADDRESS, {
+      value: ethers.parseEther("100"),
+    });
+  } else {
+    initCode = "0x";
+  }
 
-	// if need funds for depositing, uncomment below
-  await entryPoint.depositTo(PM_ADDRESS, {
-    value: ethers.parseEther("100"),
-  });
+  console.log("sender", sender);
 
   const Account = await hre.ethers.getContractFactory("Account");
   const userOp = {
