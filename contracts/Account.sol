1ab8ab249496eec93c5d7196b17c5ce3f4302740
tommyrharper <thomasrobertharper@gmail.com>
2024-07-01 13:52:56 +0100

    refactor: remove unused function params

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -3,19 +3,28 @@ pragma solidity ^0.8.24;
 
 import "@account-abstraction/contracts/core/EntryPoint.sol";
 import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "hardhat/console.sol";
+
+contract Test {
+    constructor(bytes memory sig) {
+        address recovered = ECDSA.recover(ECDSA.toEthSignedMessageHash(keccak256("wee")), sig);
+        console.log(recovered);
+    }
+}
 
 contract Account is IAccount {
     uint256 public count;
     address public owner;
 
-    constructor(address owner) {
-        owner = owner;
+    constructor(address _owner) {
+        owner = _owner;
     }
 
     function validateUserOp(
-        UserOperation calldata userOp,
-        bytes32 userOpHash,
-        uint256 missingAccountFunds
+        UserOperation calldata, // userOp
+        bytes32, // userOpHash
+        uint256 // missingAccountFunds
     ) external pure returns (uint256 validationData) {
         return 0;
     }
