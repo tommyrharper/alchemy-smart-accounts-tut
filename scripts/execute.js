d406fe2f79ba7e80edeaf7d07c90e74929eb5596
tommyrharper <thomasrobertharper@gmail.com>
2024-07-01 13:24:36 +0100

    feat: update execute

--- scripts/execute.js
+++ scripts/execute.js
@@ -4,6 +4,7 @@ const hre = require("hardhat");
 const FACTORY_NONCE = 1;
 const FACTORY_ADDRESS = "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512";
 const EP_ADDRESS = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+const PM_ADDRESS = "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0";
 
 async function main() {
   const [signer0] = await hre.ethers.getSigners();
@@ -20,18 +21,18 @@ async function main() {
   });
 
   const AccountFactory = await hre.ethers.getContractFactory("AccountFactory");
-  const initCode = "0x"; // if deploying for first time use init code below
-    // FACTORY_ADDRESS +
-    // AccountFactory.interface
-    //   .encodeFunctionData("createAccount", [address0])
-    //   .slice(2);
+  // const initCode = "0x"; // if deploying for first time use init code below
+    const initCode = FACTORY_ADDRESS +
+    AccountFactory.interface
+      .encodeFunctionData("createAccount", [address0])
+      .slice(2);
 
 	console.log("sender", sender);
 
 	// if need funds for depositing, uncomment below
-  // await entryPoint.depositTo(sender, {
-  //   value: ethers.parseEther("100"),
-  // });
+  await entryPoint.depositTo(PM_ADDRESS, {
+    value: ethers.parseEther("100"),
+  });
 
   const Account = await hre.ethers.getContractFactory("Account");
   const userOp = {
@@ -44,7 +45,7 @@ async function main() {
     preVerificationGas: 50_000,
     maxFeePerGas: hre.ethers.parseUnits("10", "gwei"),
     maxPriorityFeePerGas: hre.ethers.parseUnits("10", "gwei"),
-    paymasterAndData: "0x",
+    paymasterAndData: PM_ADDRESS,
     signature: "0x",
   };
 
